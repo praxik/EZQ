e80a4fee3424291d4b25ab9bfc12e9cab8d83820
Penn Taylor <rpenn3@gmail.com>
2014-02-17 22:34:43 +0000

    processor_fan_out now reads the contents of processor_fan_out.yml to determine how many processes to start up.

--- processor_fan_out.rb
+++ processor_fan_out.rb
@@ -2,14 +2,34 @@
 
 # This script runs EZQ::Processor in each of the 44 separate deploy subdirs.
 
+require 'yaml'
+
 #command ="echo $PWD"
+
 command = 'ruby processor.rb -c 6k_worker_queue_config.yml'
 Dir.chdir(File.dirname(__FILE__))
 
+if !File.exists?('processor_fan_out.yml')
+  warn 'processor_fan_out.yml does not exist. Aborting processor_fan_out.rb'
+  exit 1
+end
+
+y = YAML.load(File.read('processor_fan_out.yml'))
+
+if !y.kind_of?(Hash)
+  warn 'processor_fan_out.yml is formatted incorrectly. Aborting processor_fan_out.rb'
+  exit 1
+end
+
+num = y['number_of_processes'].to_i
+
 pids = []
 
-44.times do |idx|
+num.times do |idx|
   Dir.chdir("%02d" % idx) {pids << spawn(command)}
+  # To run directly in place rather than in numbered subdirs, comment
+  # out the previous line and uncomment the following:
+  #pids << spawn(command)
 end
 
 pids.each do |pid| 
