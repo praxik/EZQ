f4fb3d1837cb5e0f53d30a01687893812b022abb
Penn Taylor <rpenn3@gmail.com>
2014-02-20 21:28:52 +0000

    Minor code cleanup.

--- emit_test_jobs.rb
+++ emit_test_jobs.rb
@@ -4,9 +4,7 @@ $stdout.sync = true
 require 'json'
 
 # Emit some push_file directives
-
 puts "push_file: 6k_test.praxik,skel/08842505P7000_.skel"
-
 puts "push_file: 6k_test.praxik,soils/IA015_2550232-543033.soi"
 puts "push_file: 6k_test.praxik,soils/IA015_2550235-543040.soi"
 puts "push_file: 6k_test.praxik,soils/IA015_2550236-543041.soi"
@@ -22,36 +20,24 @@ puts "push_file: 6k_test.praxik,soils/IA153_412839-560963.soi"
 puts "push_file: 6k_test.praxik,soils/IA153_412963-560923.soi"
 puts "push_file: 6k_test.praxik,soils/IA153_412965-560925.soi"
 
-# Emit a batch of tasks
-
-#~file = File.read('test_worker_task_data.json')
-#~json = JSON.parse(file)
-#~while true
-  #~json['tasks'].each do |job| 
-    #~puts job.to_json
-    #~# Simulate some step that requires clock time
-    #~sleep(2)
-  #~end
-#~end
-
-# The file passed in on cmdline will contain nothing but an integer. This
-# simulates having a meaningful job message on which to operate. For this
-# test, we will simply read that integer and enqueue that number of jobs.
-
-max = File.read(ARGV[0]).strip.to_i
-
+# Prepare a test task
 test_task = <<-END
     {
       "Task ID" : "1",
       "mmp360 input data" : { "soi_file" : "location", "skel_file" : "location", "musym" : "soilname" , "mukey" : "keyvalue", "slope" : 10.0 }
     }
     END
-
 test_json = JSON.parse(test_task)
-count = 0
+
+# The file passed in on cmdline will contain nothing but an integer. This
+# simulates having a meaningful job message on which to operate. For this
+# test, we will simply read that integer and enqueue that number of jobs.
+max = File.read(ARGV[0]).strip.to_i
 
 1.upto(max) do |idx|
   test_json['Task ID'] = idx.to_s
   puts test_json.to_json
+  # Simulate some process requiring clock time. This is **not** necessary
+  # for the functioning of this test or the enqueuing process.
   sleep(2)
 end
