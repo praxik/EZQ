23197ed9942897250fc1e05f3049b01651de994f
Penn Taylor <rpenn3@gmail.com>
2014-03-25 23:12:37 -0500

    Initial commit of script to start up a job

---
+++ Utils/start_job.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require 'bundler/setup'
+require 'yaml'
+require 'aws-sdk'
+
+credentials = YAML.load(File.read('credentials.yml'))
+AWS.config(credentials)
+job_queue = AWS::SQS.new.queues.named('6k_job_test_44')
+job_queue.send_message('Start')
+
+puts 'Sent message to start job.'
