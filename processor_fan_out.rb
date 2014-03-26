f1faa6c62a16b1e99963fa4b862dfad9d8ea8b13
Penn Taylor <rpenn3@gmail.com>
2014-03-25 23:01:05 -0500

    processor_fan_out now attempts to get number of processes from instance userdata

--- processor_fan_out.rb
+++ processor_fan_out.rb
@@ -2,30 +2,47 @@
 
 # This script runs EZQ::Processor in each of the 44 separate deploy subdirs.
 
+require 'bundler/setup'
 require 'yaml'
-
-#command ="echo $PWD"
+require 'aws-sdk'
+require 'net/http'
+require 'uri'
 
 command = 'ruby processor.rb -c 6k_worker_queue_config.yml'
 Dir.chdir(File.dirname(__FILE__))
 
-if !File.exists?('processor_fan_out.yml')
-  warn 'processor_fan_out.yml does not exist. Aborting processor_fan_out.rb'
-  exit 1
+@num = 1
+
+begin
+  puts 'Looking for number of processors to run in userdata...'
+  credentials = YAML.load(File.read('credentials.yml'))
+  AWS.config(credentials)
+  uri = URI.parse("http://169.254.169.254/latest/meta-data/instance-id")
+  instance_id = Net::HTTP.get_response(uri).body
+  instance = AWS::EC2.new.instances[instance_id]
+  raise unless instance.exists?#Using this pattern because either of the 
+  # two previous calls can raise an exception, and I want to do exactly the
+  # same thing in any of these cases.
+  userdata = YAML.load(instance.user_data)
+  @num = userdata['number_of_processes'].to_i
+rescue
+  puts 'Number of desired processes not found in userdata. Falling back to number in processor_fan_out.yml.'
+  if !File.exists?('processor_fan_out.yml')
+    warn 'processor_fan_out.yml does not exist. Aborting processor_fan_out.rb'
+    exit 1
+  end
+  y = YAML.load(File.read('processor_fan_out.yml'))
+  if !y.kind_of?(Hash)
+    warn 'processor_fan_out.yml is formatted incorrectly. Aborting processor_fan_out.rb'
+    exit 1
+  end
+  @num = y['number_of_processes'].to_i
 end
 
-y = YAML.load(File.read('processor_fan_out.yml'))
-
-if !y.kind_of?(Hash)
-  warn 'processor_fan_out.yml is formatted incorrectly. Aborting processor_fan_out.rb'
-  exit 1
-end
-
-num = y['number_of_processes'].to_i
 
 pids = []
 
-num.times do |idx|
+@num.times do |idx|
   command += " --log ./processor_" + "%02d" % idx + ".log"
   #Dir.chdir("%02d" % idx) {pids << spawn(command)}
   # To run directly in place rather than in numbered subdirs, comment
