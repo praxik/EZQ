d22ac488f37eecfab60337293c4373e1b4b56452
Penn Taylor <rpenn3@gmail.com>
2014-03-25 23:14:38 -0500

    Point start_job to the correct creds file

--- Utils/start_job.rb
+++ Utils/start_job.rb
@@ -4,7 +4,7 @@ require 'bundler/setup'
 require 'yaml'
 require 'aws-sdk'
 
-credentials = YAML.load(File.read('credentials.yml'))
+credentials = YAML.load(File.read('../credentials.yml'))
 AWS.config(credentials)
 job_queue = AWS::SQS.new.queues.named('6k_job_test_44')
 job_queue.send_message('Start')
