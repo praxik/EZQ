f2e9ba6142cc8e67315f96689643e3bdee3fe70b
Penn Taylor <rpenn3@gmail.com>
2014-04-17 12:56:57 -0500

    Start job now pulls job details from file.

--- Utils/start_job.rb
+++ Utils/start_job.rb
@@ -7,6 +7,15 @@ require 'aws-sdk'
 credentials = YAML.load(File.read('credentials.yml'))
 AWS.config(credentials)
 job_queue = AWS::SQS.new.queues.named('6k_job_test_44')
-job_queue.send_message('Start')
 
-puts 'Sent message to start job.'
+job_hash = {}
+man_files = []
+
+job_config = ARGV[0] != nil ? ARGV[0] : 'job_config.yml'
+
+job_hash = YAML.load(File.read(job_config))
+
+job_queue.send_message(job_hash.to_yaml)
+
+puts 'Sent following message to job queue:'
+puts job_hash.to_yaml
