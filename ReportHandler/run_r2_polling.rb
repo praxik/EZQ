ae4b8e3d402f444e69c14805cfb9006a28ccd616
Penn Taylor <rpenn3@gmail.com>
2014-05-06 16:08:36 -0500

    Logic to send dominant critical job back to worker, wait for results, and send off to pdf_report.rb.

--- ReportHandler/run_r2_polling.rb
+++ ReportHandler/run_r2_polling.rb
@@ -61,6 +61,9 @@ class RusleReport < EZQ::Processor
       Array(msg).each do |item|
         process_message(item)
         if @rr_remaining_tasks.empty?
+          deal_with_report if @gen_dom_crit_report # Breaks out of recursion in
+                # addition to simply seeing whether this step must be performed.
+                                                   
           AWS::SQS.new.queues.named("#{@rr_job_id}").delete
           exit(0)
         end
@@ -76,15 +79,16 @@ class RusleReport < EZQ::Processor
         Their IDs are: #{@rr_remaining_tasks}
         END
       send_error(errm)
-    else
-      deal_with_report if @gen_dom_crit_report
     end
   end
 
 
 
-
+  # TODO: clean this mess up. Break out functionality into smaller, digestible
+  # pieces.
   def deal_with_report
+    # 6k_aggregator c++ app will look at the soil geojson and tell us which
+    # is the dominant critical soil
     IO.popen('./6k_aggregator',"#{@rr_job_id}","#{@soil_geojson}") do |io|
       while !io.eof?
         dom_crit_id = io.gets
@@ -97,12 +101,50 @@ class RusleReport < EZQ::Processor
     i_data = {}
     inputs.each do |input|
       i_data = JSON.parse(input)
-      break if i_data['cell_id'] == dom_crit_id
+      break if i_data['task_id'] == dom_crit_id
     end
     # Add kv pair report : true to the top level of this structure,
     i_data['report'] = true
-    # Form up an EZQ header for get_s3_files using the values in s3_files key
-    # Place task back into worker task queue. Then... start polling again.
+    # Form up an EZQ header for get_s3_files.
+    # We rely on the fact that we know exactly how the the values of the soiR2
+    # and ifcWeps keys map to filenames. A better solution would be to put a
+    # Ruby wrapper around the worker process that pulls in and escapes the
+    # full message, with header, and appends it to the result output as a new
+    # field. Then deal_with_report doesn't have to know all the gory details
+    # of the non-report worker result messages.
+    soiR2 = "share/leaf/rusle2/#{i_data['soiR2']}"
+    ifcWeps = "share/leaf/weps/weps.wjr/#{i_data['ifcWeps']}"
+    preamble = {}
+    ezq = {}
+    preamble['EZQ'] = ezq
+    ezq['result_queue_name'] = @rr_job_id
+    pushed_files = []
+    pushed_files.push(Hash['bucket'=>'6k_test.praxik','key'=>soiR2])
+    pushed_files.push(Hash['bucket'=>'6k_test.praxik','key'=>ifcWeps])
+    ezq['get_s3_files'] = pushed_files
+    preamble = preamble.to_yaml
+    preamble += "...\n"
+
+    msg = "#{preamble}#{i_data.to_json}"    
+    
+    # Place task back into worker task queue.
+    # FIXME: hardcoding the queue name like this will break spot instance
+    # workflows.
+    task_q_name = '6k_task_test_44'
+    queue = AWS::SQS.new.queues.named(task_q_name)
+    
+    if !queue.exists?
+      @logger.fatal "No queue named #{task_q_name} exists."
+      raise "No queue named #{task_q_name} exists."
+    end
+    queue.send_message(msg)
+
+    # Start polling again.
+    @dont_hit_disk = false # We want the results to be written this time!
+    @process_command = "ruby pdf_report.rb $input_file"
+    @gen_dom_crit_report = false # This will cause a graceful exit after
+                                 # pdf_report finishes.
+    start
   end
 
 end
