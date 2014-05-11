f172a07f13b709874fef2fe897a3ccc52e99a260
Penn Taylor <rpenn3@gmail.com>
2014-05-11 13:46:41 -0500

    Corrected start_job logic.

--- Utils/start_job.rb
+++ Utils/start_job.rb
@@ -9,13 +9,16 @@ AWS.config(credentials)
 job_queue = AWS::SQS.new.queues.named('6k_job_test_44')
 
 job_hash = {}
-man_files = []
 
 job_config = ARGV[0] != nil ? ARGV[0] : 'job_config.yml'
 
-job_hash = YAML.load(File.read(job_config))
+msg = File.read(job_config)
 
-job_queue.send_message(job_hash.to_yaml)
+#job_hash = YAML.load(File.read(job_config))
+
+#job_queue.send_message(job_hash.to_yaml)
+job_queue.send_message(msg)
 
 puts 'Sent following message to job queue:'
-puts job_hash.to_yaml
+#puts job_hash.to_yaml
+puts msg
