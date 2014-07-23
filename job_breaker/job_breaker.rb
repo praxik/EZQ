64ba6f1b0f123a4661ee7e840a1692bada66c32c
Penn Taylor <rpenn3@gmail.com>
2014-07-23 15:20:24 -0500

    Fix unescaping for backslashes.

--- job_breaker/job_breaker.rb
+++ job_breaker/job_breaker.rb
@@ -289,9 +289,9 @@ class Job_Breaker
   # 
   def unescape(str)
     # Escape all the things
-    str.gsub(/\\(?:([#{UNESCAPES.keys.join}])|u([\da-fA-F]{4}))|0?x([\da-fA-F]{2})/) {
+    str.gsub(/\\(?:([#{UNESCAPES.keys.join}])|u([\da-fA-F]{4}))|\\0?x([\da-fA-F]{2})/) {
       if $1
-        UNESCAPES[$1] # escape characters
+        if $1 == '\\' then '\\' else UNESCAPES[$1] end
       elsif $2 # escape \u0000 unicode
         ["#$2".hex].pack('U*')
       elsif $3 # escape \0xff or \xff
@@ -300,10 +300,12 @@ class Job_Breaker
     }
   end
 
-  UNESCAPES = { 'a' => "\x07", 'b' => "\x08", 't' => "\x09",
-                'n' => "\x0a", 'v' => "\x0b", 'f' => "\x0c",
-                'r' => "\x0d", 'e' => "\x1b", '\\' => '\\',
-                "\"" => "\x22", "'" => "\x27" }
+  UNESCAPES = {
+    'a' => "\x07", 'b' => "\x08", 't' => "\x09",
+    'n' => "\x0a", 'v' => "\x0b", 'f' => "\x0c",
+    'r' => "\x0d", 'e' => "\x1b", "\\\\" => "\x5c",
+    "\"" => "\x22", "'" => "\x27"
+}
   
 end #class
 end #module
