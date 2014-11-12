53312befd84f4e32b8ffe925e96bec14079895d3
Penn Taylor <rpenn3@gmail.com>
2014-11-11 23:10:10 -0600

    Moved EZQ::Processor over to using Local/Loggly dual logs.

--- processor.rb
+++ processor.rb
@@ -7,13 +7,14 @@ require 'zlib'
 require 'base64'
 require 'net/http'
 require 'uri'
-require 'logger'
+#require 'logger'
 require 'digest/md5'
 require 'fileutils'
 require 'zip'
 
 require './x_queue'
 require_relative './ezqlib'
+require_relative './dual_log'
 
 
 module EZQ
@@ -931,6 +932,9 @@ if __FILE__ == $0
   queue = nil
   error_queue = nil
   log_file = STDOUT
+  loggly_token = nil
+  loggly_severity = 'info'
+  app_name = 'EZQ::Processor'
   op = OptionParser.new do |opts|
     opts.banner = "Usage: processor.rb [options]"
 
@@ -943,8 +947,17 @@ if __FILE__ == $0
     opts.on("-l", "--log [LOG_FILE]","Log to file LOG_FILE") do |file|
       log_file = file
     end
-    opts.on("-s", "--log_severity [SEVERITY]","Set log severity to one of: unknown, fatal, error, warn, info, debug. Default = info") do |s|
+    opts.on("-s", "--log_severity [SEVERITY]","Set local log severity to one of: unknown, fatal, error, warn, info, debug. Default = info") do |s|
       severity = s
+    end
+    opts.on("-t", "--token [LOGGLY_TOKEN]","Loggly token. Turns on logging to loggly") do |token|
+      loggly_token = token
+    end
+    opts.on("-u", "--loggly_severity [SEVERITY]","Set loggly severity to one of: unknown, fatal, error, warn, info, debug. Default = info") do |s|
+      loggly_severity = s
+    end
+    opts.on("-n", "--app_name [APP_NAME]","Application name to use with Loggly.") do |name|
+      app_name = name
     end
     opts.on("-r", "--credentials [CREDS_FILE]","Use credentials file CREDS_FILE. Defaults to credentials.yml if not specified.") do |file|
       creds_file = file
@@ -969,22 +982,26 @@ if __FILE__ == $0
   begin
     puts "EZQ.Processor started.\n\n" unless quiet
     if log_file != STDOUT
-      lf = File.new(log_file, 'a')
-      lf.sync = true
-      log = Logger.new(lf)
-      $stderr = lf
+      log_file = File.new(log_file, 'a')
+      log_file.sync = true
+      log = Logger.new(log_file)
+      $stderr = log_file
     else
-      if !quiet
-        log = Logger.new(STDOUT)
-      else
+      if quiet
         log_file = RUBY_PLATFORM =~ /mswin|mingw/ ? 'NUL:' : '/dev/null'
-        log = Logger.new(log_file)
       end
     end
     s_map = {'unknown'=>Logger::UNKNOWN,'fatal'=>Logger::FATAL,
              'error'=>Logger::ERROR,'warn'=>Logger::WARN,
              'info'=>Logger::INFO,'debug'=>Logger::DEBUG}
-    log.level = s_map[severity]
+
+    log = DualLogger.new({:progname=>app_name,
+                          :ip=>EZQ.get_local_ip(),
+                          :filename=>log_file,
+                          :local_level=>s_map[severity],
+                          :loggly_token=>loggly_token,
+                          :loggly_level=>s_map[loggly_severity],
+                          :pid=>Process.pid})
     
 
     if !File.exists?(creds_file)
