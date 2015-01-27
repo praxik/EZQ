42b8f2d19a25765fb9ffa6b7ae67c1daa92e0833
Penn Taylor <rpenn3@gmail.com>
2015-01-26 23:43:07 -0600

    Extract bbox and wkid from geojson rather than require as explicit input.

--- landsat/landsat_scene_finder.rb
+++ landsat/landsat_scene_finder.rb
@@ -1,13 +1,9 @@
 #!/usr/bin/env ruby
 
-# Original set of outFields:
-#  'sceneID,sensor,acquisitionDate,dateUpdated,path,row,PR,cloudCover,
-#   sunElevation,sunAzimuth,receivingStation,sceneStartTime,month,year,
-#   OBJECTID,dayOfYear,dayOrNight,browseURL'
-
 require 'net/http'
 require 'json'
 require_relative './landsatlook_date_converter'
+require_relative './ezqlib'
 
 module LsSceneFinder
 
@@ -16,12 +12,9 @@ module LsSceneFinder
   #                 in yyyy-mm-dd format
   # @param [String] end_date End of scene date range
   #                 in yyyy-mm-dd format
-  # @param [Numeric] Maximum allowed percentage cloud cover
-  # @param [Numeric] xmin Bounding box minumum x coordinate
-  # @param [Numeric] xmax Bounding box maximum x coordinate
-  # @param [Numeric] ymin Bounding box minumum y coordinate
-  # @param [Numeric] ymax Bounding box maximum y coordinate
-  # @param [Integer] wkid WKID of the input spatial representation
+  # @param [Numeric] cloud_cover Maximum allowed percentage cloud cover
+  # @param [String] aoi_file Path to geojson file containing
+  #                 area of interest
   # @return [Array(Hash)] Array of Hash objects conforming to
   #         {:sceneID => String,
   #          :cloudCover => Integer,
@@ -29,20 +22,15 @@ module LsSceneFinder
   #          :browseURL => String}
   #          If no valid scenes are returned, the array will
   #          simply be empty.
-  def LsSceneFinder.find_scenes(
-                                start_date = '2014-10-01',
-                                end_date = '2014-10-31',
-                                cloud_cover = 20,
-                                xmin = 0,
-                                xmax = 0,
-                                ymin = 0,
-                                ymax = 0,
-                                wkid = 4326 )
+  def LsSceneFinder.find_scenes( start_date = '2014-10-01',
+                                 end_date = '2014-10-31',
+                                 cloud_cover = 20,
+                                 aoi_file = '' )
 
+    wkid = extract_wkid(aoi_file)
+    [xmin,ymin,xmax,ymax] = get_bbox(aoi_file)
     response = get_json(start_date,end_date,cloud_cover,xmin,xmax,ymin,ymax,wkid)
-
     raise(response.message) if response.code != '200'
-
     return extract(response.body)
   end
 
@@ -50,8 +38,35 @@ module LsSceneFinder
 
   private
 
+
+  def extract_wkid(file)
+    j = JSON.parse(File.read(file))
+    crs = j.fetch('crs',nil)
+    return 4326 if !crs
+    name = crs["properties"]["name"]
+    return name.split(':').last.to_i
+  end
+
+
+
+  def get_bbox(file)
+    cmd = "ogrinfo -al -so -ro #{file} | grep Extent"
+    extent = EZQ.exec_cmd(cmd)
+
+    # extent contains a string like this:
+    # Extent: (-92.880077, 43.269621) - (-92.870378, 43.274183)
+
+    extent.gsub!('Extent: ','')
+    extent.slice!(0)
+    extent.slice!(extent.length - 1)
+    pairs = extent.split(') - (')
+    return pairs.map{|p| p.split(', ')}.flatten.map{|c| c.to_f}
+  end
+
+
+
   # Get scene metadata from LandsatLook server
-  def LsSceneFinder.get_json( start_date,end_date,cloud_cover,xmin,xmax,ymin,ymax,wkid)
+  def LsSceneFinder.get_json(start_date,end_date,cloud_cover,xmin,xmax,ymin,ymax,wkid)
 
     uri = URI("http://landsatlook.usgs.gov/arcgis/rest/services/LandsatLook/ImageServer/query")
 
@@ -113,47 +128,35 @@ end # module
 if __FILE__ == $0
 
   if ['-h','--help'].include?(ARGV[0])
-    puts "\nUsage: landsat_scene_finder [-f FILE] [START_DATE]
-          [END_DATE] [CLOUD_COVER] [XMIN] [XMAX]
-          [YMIN] [YMAX] [WKID]
+    puts "\nUsage: landsat_scene_finder [START_DATE]
+          [END_DATE] [CLOUD_COVER] [AOI_GEOJSON_FILE]
 
-          Seriously though, do yourself a favor and shove all the
-          positional arguments into a file and invoke this thing
-          like so:
-
-          landsat_scene_finder -f arg_file\n\n"
+          START DATE and END DATE should be in form yyyy-mm-dd
+          CLOUD_COVER should be an integer"
     exit
   end
 
   vars = []
-
-  if ARGV[0] == '-f'
-    v = File.readlines(ARGV[1]).first
-    vars = v.split(' ')
-  else
-     vars = ARGV
-  end
+  vars = ARGV
 
 
   sd = vars[0] ? vars[0] : '2014-01-01'
   ed = vars[1] ? vars[1] : '2014-08-31'
   cc = vars[2] ? vars[2] :  20
 
+  aoi = vars[3] ? vars[3] : 'field_Voight_10_1415115678.geojson'
+
   # Voight North
-  xm = vars[3] ? vars[3] : -92.880077
-  xx = vars[4] ? vars[4] : -92.870378
-  ym = vars[5] ? vars[5] : 43.269621
-  yx = vars[6] ? vars[6] : 43.274183
-  wk = vars[7] ? vars[7] : 4326
+#   xm = vars[3] ? vars[3] : -92.880077
+#   xx = vars[4] ? vars[4] : -92.870378
+#   ym = vars[5] ? vars[5] : 43.269621
+#   yx = vars[6] ? vars[6] : 43.274183
+#   wk = vars[7] ? vars[7] : 4326
 
   data = LsSceneFinder.find_scenes( start_date = sd,
                                     end_date = ed,
                                     cloud_cover = cc.to_i,
-                                    xmin = xm.to_f,
-                                    xmax = xx.to_f,
-                                    ymin = ym.to_f,
-                                    ymax = yx.to_f,
-                                    wkid = wk.to_i )
+                                    aoi )
 
   puts data
 end
