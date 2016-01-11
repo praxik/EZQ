84f3f040acc7fed7c8a87f9bc4c89224fce4e147
Penn Taylor <rpenn3@gmail.com>
2016-01-11 13:39:59 -0600

    Syntax fix for SNS

--- gem/lib/ezq/utils/sns.rb
+++ gem/lib/ezq/utils/sns.rb
@@ -5,7 +5,7 @@ module EZQ
   module SNS
 
     def SNS.publish(topic, message)
-      Aws::SNS.Topic.new(topic).publish(message: message)
+      Aws::SNS::Topic.new(topic).publish(message: message)
     end
 
   end
