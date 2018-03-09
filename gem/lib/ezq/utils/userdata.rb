5476598fda16bcbfc8f5043843db99cde4e5200f
Doug McCorkle <doug@agsolver.com>
2018-03-09 16:27:27 -0600

    initial commit to provide helper utilities for loading userdata on lambdas.

---
+++ gem/lib/ezq/utils/userdata.rb
@@ -0,0 +1,41 @@
+require 'base64'
+
+module EZQ
+  module UserData
+    def self.load
+      userdata = nil
+
+      if ENV.key?('USERDATA')
+        if ENV.key?('USERDATA_ENCODING') && ENV['USERDATA_ENCODING'] == 'base64'
+          # userdata is Base64-encoded
+          userdata = YAML.load(Base64.decode64(ENV['USERDATA']))
+        else
+          # assume USERDATA contains plain text
+          userdata = YAML.load(ENV['USERDATA'])
+        end
+      else
+        search_paths = [
+          '/tmp/userdata.yml',
+          File.join(__dir__, 'userdata.yml'),
+          File.join(Dir.pwd, 'userdata.yml')
+        ]
+
+        search_paths.each do |path|
+          begin
+            File.open(path, File::RDONLY) do |f|
+              userdata = YAML.load(f.read)
+            end
+
+            break
+          rescue Errno::ENOENT
+            next
+          end
+        end
+      end
+
+      raise RuntimeError, 'No userdata found' if userdata.nil?
+
+      userdata
+    end
+  end
+end
